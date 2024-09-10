40d80ebb7e6f9581e9518cb194368bcc07cd2d26
el-crag <azertyggmf@outlook.com>
2024-09-10 14:03:28 -0500

    Main file with export

---
+++ src/fractuyo.js
@@ -0,0 +1,18 @@
+import Invoice from "./receipt/Invoice";
+import Item from "./receipt/Item";
+import Share from "./receipt/Share";
+import Charge from "./receipt/Charge";
+
+import Person from "./person/Person";
+import Taxpayer from "./person/Taxpayer";
+import Identification from "./person/Identification";
+
+export {
+	Invoice,
+	Item,
+	Share,
+	Charge,
+	Person,
+	Taxpayer,
+	Identification
+}
