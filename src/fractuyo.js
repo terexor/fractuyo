039515b64350798a108389816508fa80d209eeeb
el-crag <azertyggmf@outlook.com>
2024-09-10 22:18:37 -0500

    Added extension at importing file

--- src/fractuyo.js
+++ src/fractuyo.js
@@ -1,11 +1,11 @@
-import Invoice from "./receipt/Invoice";
-import Item from "./receipt/Item";
-import Share from "./receipt/Share";
-import Charge from "./receipt/Charge";
+import Invoice from "./receipt/Invoice.js";
+import Item from "./receipt/Item.js";
+import Share from "./receipt/Share.js";
+import Charge from "./receipt/Charge.js";
 
-import Person from "./person/Person";
-import Taxpayer from "./person/Taxpayer";
-import Identification from "./person/Identification";
+import Person from "./person/Person.js";
+import Taxpayer from "./person/Taxpayer.js";
+import Identification from "./person/Identification.js";
 
 export {
 	Invoice,
