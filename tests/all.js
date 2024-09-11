c330d320bb4f61a954d62fc7ccbcaf19854387d0
el-crag <azertyggmf@outlook.com>
2024-09-11 13:05:07 -0500

    Unit test content

--- tests/all.js
+++ tests/all.js
@@ -3,3 +3,31 @@ import { Invoice, Item, Share, Charge, Person, Taxpayer, Identification } from '
 if (typeof Invoice === 'undefined') {
 	throw new Error('Invoice is not defined');
 }
+else {
+	const customer = new Person()
+	customer.setName("Garmur")
+
+	const taxpayer = new Taxpayer()
+	taxpayer.setIdentification(new Identification(6, "20606829265"))
+
+	const invoice = new Invoice(taxpayer, customer)
+	invoice.setCurrencyId("USD")
+	invoice.setSerie("F000")
+	invoice.setNumeration(19970601)
+	invoice.setOrderReference("test-002")
+	invoice.setOrderReferenceText("Testing library to generate invoice")
+
+	const product = new Item("This is description for item")
+	product.setUnitCode("NIU")
+	product.setClassificationCode("82101500")
+	product.setIscPercentage(0)
+	product.setIgvPercentage(18)
+	product.setQuantity(3)
+	product.setUnitValue(100.00)
+	product.calcMounts()
+
+	invoice.addItem(product)
+
+	console.log("Invoice:", invoice.getId(true))
+	console.log("QR data:", invoice.getDataQr())
+}
