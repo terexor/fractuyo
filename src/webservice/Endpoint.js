f387f895b79774733c63ed50103feca9cc2df7ca
el-crag <azertyggmf@outlook.com>
2024-10-03 17:02:22 -0500

    REST endpoints

--- src/webservice/Endpoint.js
+++ src/webservice/Endpoint.js
@@ -1,10 +1,20 @@
+import Rest from "./Rest.js"
+
 class Endpoint {
 	// true for deployment; false for test
 	static #mode = false
 
+	static #token
+
+	static #offset = 4
+
 	static INDEX_INVOICE = 0
 	static INDEX_RETENTION = 1
-	static INDEX_DESPATCH = 2
+
+	static INDEX_TOKEN = 1 << Endpoint.#offset
+	static INDEX_SEND = 2 << Endpoint.#offset
+	static INDEX_STATUS = 3 << Endpoint.#offset
+	static #REST_MASK = 0b111 << Endpoint.#offset // despatch is here
 
 	static setMode(mode) {
 		Endpoint.#mode = mode
@@ -32,15 +42,46 @@ class Endpoint {
 		{ // retention
 			deploy: "https://e-factura.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService",
 			test: "https://e-beta.sunat.gob.pe/ol-ti-itemision-otroscpe-gem-beta/billService"
-		},
-		{ // despatch
-			deploy: "https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem/billService",
-			test: "https://e-beta.sunat.gob.pe/ol-ti-itemision-guia-gem-beta/billService"
 		}
 	]
 
+	/**
+	 * Currently for despatchs but looks like for any document.
+	 */
+	static #restUrls = [
+		{ // despatch token
+			deploy: "https://api-seguridad.sunat.gob.pe/v1/clientessol/<client_id>/oauth2/token",
+			test: "https://gre-test.nubefact.com/v1/clientessol/<client_id>/oauth2/token"
+		},
+		{ // despatch send
+			deploy: "https://api-cpe.sunat.gob.pe/v1/contribuyente/gem/comprobantes/", // ending: {numRucEmisor}-{codCpe}-{numSerie}-{numCpe}
+			test: "https://gre-test.nubefact.com/v1/contribuyente/gem/comprobantes/"
+		},
+		{ // despatch status
+			deploy: "https://api-cpe.sunat.gob.pe/v1/contribuyente/gem/comprobantes/envios/", // ending: {numTicket}
+			test: "https://gre-test.nubefact.com/v1/contribuyente/gem/comprobantes/envios/"
+		},
+	]
+
+	static getUrl(service, mode) {
+		// Use what is in current scope
+		if (mode == undefined) {
+			Endpoint.#mode
+		}
+
+		if (service == Endpoint.INDEX_INVOICE || service == Endpoint.INDEX_RETENTION) {
+			return mode ? Endpoint.#urls[service].deploy : Endpoint.#urls[service].test
+		}
+
+		if ((service & Endpoint.#REST_MASK) != 0) {
+			service >>= Endpoint.#offset
+			--service
+			return mode ? Endpoint.#restUrls[service].deploy : Endpoint.#restUrls[service].test
+		}
+	}
+
 	static async fetch(service, body) {
-		const url = Endpoint.#mode ? Endpoint.#urls[service].deploy : Endpoint.#urls[service].test
+		const url = Endpoint.getUrl(service)
 
 		const response = await fetch(url, {
 			method: "POST",
@@ -50,6 +91,45 @@ class Endpoint {
 		const responseText = await response.text()
 
 		return responseText
+	}
+
+	static async fetchSend(body, receipt) {
+		const url = Endpoint.getUrl(Endpoint.INDEX_SEND).concat(`${receipt.taxpayer.getIdentification().getNumber()}-${receipt.getId(true)}`)
+
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {"Content-Type": "application/json", "Authorization": "Bearer " + Endpoint.#token},
+			body: body
+		})
+		const responseJson = await response.json()
+
+		return responseJson
+	}
+
+	static async fetchToken(taxpayer) {
+		const url = Endpoint.getUrl(Endpoint.INDEX_TOKEN).replace("<client_id>", taxpayer.getSolId())
+
+		const data = Rest.generateToken(taxpayer)
+
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {"Content-Type": "application/x-www-form-urlencoded"},
+			body: data
+		})
+		const responseJson = await response.json()
+
+		// Hold value
+		Endpoint.#token = responseJson.access_token
+
+		return responseJson
+	}
+
+	static get token() {
+		return Endpoint.#token
+	}
+
+	static set token(token) {
+		Endpoint.#token = token
 	}
 }
 
