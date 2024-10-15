e190dd926e65ec0b8e4e0cae8540b8486de56e37
el-crag <azertyggmf@outlook.com>
2024-10-15 13:27:03 -0500

    More metadata for vehicle

--- src/shipment/Vehicle.js
+++ src/shipment/Vehicle.js
@@ -1,6 +1,12 @@
 class Vehicle {
 	#identity // registration, placa, matrícula
 
+	#registrationIdentity // circulation number card
+
+	#authorization // authorization number
+
+	#departmentCode // state entity, agency
+
 	constructor(identity) {
 		this.setIdentity(identity)
 	}
@@ -11,6 +17,30 @@ class Vehicle {
 
 	get identity() {
 		return this.#identity
+	}
+
+	setRegistrationIdentity(identity) {
+		this.#registrationIdentity = identity
+	}
+
+	get registrationIdentity() {
+		return this.#registrationIdentity
+	}
+
+	setAuthorization(authorization) {
+		this.#authorization = authorization
+	}
+
+	get authorization() {
+		return this.#authorization
+	}
+
+	setDepartmentCode(code) {
+		this.#departmentCode = code
+	}
+
+	get departmentCode() {
+		return this.#departmentCode
 	}
 }
 
