440149869b0a04c6e8ebdb933491b18162872a8e
Garmur <george.garro@hotmail.com>
2026-06-23 21:16:51 -0500

    Wrapper generator

--- src/receipt/xml/TagsGenerator.js
+++ src/receipt/xml/TagsGenerator.js
@@ -5,6 +5,15 @@ import Receipt from "../Receipt.js"
  * Similar in declaration to NodesGenerator's methods, but definitions.
  */
 class TagsGenerator {
+	static generateUpperWrapper(document) {
+		return `\
+<?xml version="1.0" encoding="utf-8"?>
+<${document.name} xmlns="urn:oasis:names:specification:ubl:schema:xsd:${document.name}-2" xmlns:cac="${Receipt.namespaces.cac}" xmlns:cbc="${Receipt.namespaces.cbc}" xmlns:ds="${Receipt.namespaces.ds}" xmlns:ext="${Receipt.namespaces.ext}">`
+	}
+
+	static generateLowerWrapper(document) {
+		return `</${document.name}>`
+	}
 }
 
 export default TagsGenerator
