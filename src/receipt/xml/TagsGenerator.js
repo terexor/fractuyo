aefbbc8ed6cc9ceba04fd2a4448a4b0e8b3994f6
Garmur <george.garro@hotmail.com>
2026-06-23 21:16:01 -0500

    Class to create tags strings instead nodes

---
+++ src/receipt/xml/TagsGenerator.js
@@ -0,0 +1,10 @@
+import Receipt from "../Receipt.js"
+
+/**
+ * Generation of XML nodes using string literals.
+ * Similar in declaration to NodesGenerator's methods, but definitions.
+ */
+class TagsGenerator {
+}
+
+export default TagsGenerator
